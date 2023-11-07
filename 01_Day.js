fe68b818df2e2e70480b0f9cb670c2010b2a02db
Mukul Deval <118657142+Mukul-Deval1999@users.noreply.github.com>
2023-11-07 10:30:40 +0530

    Create 01_Day.js
    solved day1 question of challenge

---
+++ 01_Day.js
@@ -0,0 +1,12 @@
+//Program to check number is odd or even
+
+function isEvenOdd(number){
+if(number% 2===0){
+    console.log(`${number} is even`);
+}else{
+    console.log(`${number} is odd`);
+}}
+
+isEvenOdd(8)
+
+
