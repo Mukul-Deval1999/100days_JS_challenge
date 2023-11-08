e671bb2c3f1248580bbb413585fe1d99e3b50618
Mukul Deval <118657142+Mukul-Deval1999@users.noreply.github.com>
2023-11-08 18:15:45 +0530

    Day_03
    Program to Find the Square Root of given Number

---
+++ 03_Day.js
@@ -0,0 +1,10 @@
+//Program to Find the Square Root of given Number
+const number=5;
+function findSquareRoot(number) {
+    if (number < 0) {
+      return NaN; 
+  
+    return (number); 
+  }
+  const result = findSquareRoot(16);
+  console.log(result); }
