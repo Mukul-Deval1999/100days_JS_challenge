a1612767658b74714b5d8792c3495d0252cef16f
Mukul Deval <118657142+Mukul-Deval1999@users.noreply.github.com>
2023-11-09 18:08:00 +0530

    Day_4
    Program to Swap Two Variables values

---
+++ 04_Day.js
@@ -0,0 +1,15 @@
+// Program to Swap Two Variables values
+let a = 5;
+let b = 10;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("After swapping:");
+console.log("a =", a);
+console.log("b =", b);
+
+
+
+
