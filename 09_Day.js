d6a8a52fe30194511ca7372a49f4fe1b5c60dd82
Mukul Deval <118657142+Mukul-Deval1999@users.noreply.github.com>
2023-11-16 15:21:10 +0530

    09_Day
    Program to Check if a number is Positive, Negative, or Zero

---
+++ 09_Day.js
@@ -0,0 +1,11 @@
+//Program to Check if a number is Positive, Negative, or Zero
+let number=-200;
+if(number<0){
+    console.log("This Is Negetive Number")
+}else if(number>0){
+    console.log("This is Positive")
+}else if (number===0){
+    console.log("This is Zero")
+}else{
+    console.log("Write correct value")
+}
