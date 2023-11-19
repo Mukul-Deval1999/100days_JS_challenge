8cf63a6c2c69e820a5e855c012a34e2ee6f7de98
Mukul Deval <118657142+Mukul-Deval1999@users.noreply.github.com>
2023-11-19 09:31:50 +0530

    Day_10
    Program to Check Prime Number

---
+++ 10_Days.js
@@ -0,0 +1,20 @@
+//Program to Check Prime Number
+function isPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  const number = 11;
+  if (isPrime(number)) {
+    console.log(number + " is a prime number.");
+  } else {
+    console.log(number + " is not a prime number.");
+  }
+  
